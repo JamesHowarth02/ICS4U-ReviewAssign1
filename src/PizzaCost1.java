fdf210d673a1a6befdca0b06b7dfd1fc3104d54e
jame1850 <jame1850@ISS2TNRKH2.scdsb.pri>
2020-09-15 12:13:37 -0400

    Added some variables.

--- src/PizzaCost1.java
+++ src/PizzaCost1.java
@@ -9,6 +9,5 @@ public class PizzaCost1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         final double labor = 1.5;
-        final double heat = 0.75;
     }
 }
