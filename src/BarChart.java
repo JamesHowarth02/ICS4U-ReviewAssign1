7876db8c38bcd14b729135373557789e5184d42c
jame1850 <jame1850@ISS2TNRKH2.scdsb.pri>
2020-09-15 11:32:41 -0400

    Init.

---
+++ src/BarChart.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jame1850
+ */
+import TurtleGraphics.Pen;
+public class BarChart {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/Momentum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/***
+ *
+ * @author jame1850
+ */
+public class Momentum {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mass, velocity, momentum;
+        
+        System.out.println("Please input the mass: ");
+        mass = scan.nextDouble();
+        System.out.println("Please input the velocity: ");
+        velocity = scan.nextDouble();
+        
+        momentum = mass * velocity;
+        System.out.println(String.format("Calculated Momentum: %.2f", momentum));
+    }
+    
+}
