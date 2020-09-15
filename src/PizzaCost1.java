601c1646c212f681a4b8144278e81e8ccf25e817
jame1850 <jame1850@ISS2TNRKH2.scdsb.pri>
2020-09-15 12:09:26 -0400

    Added some variables.

--- src/PizzaCost1.java
+++ src/PizzaCost1.java
@@ -1,20 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.util.Scanner;
 
 /**
  *
  * @author jame1850
  */
 public class PizzaCost1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        final double labor = 1.5;
+        final double heat = 0.75;
     }
-    
 }
