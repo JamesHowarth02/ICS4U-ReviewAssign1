42bf91caceb8a742cfb2eae7a6c5930c7c7bf759
jame1850 <jame1850@ISS2TNRKH2.scdsb.pri>
2020-09-15 12:31:39 -0400

    Somewhat finished Pizza Cost.

--- src/PizzaCost1.java
+++ src/PizzaCost1.java
@@ -9,5 +9,16 @@ public class PizzaCost1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         final double labor = 1.5;
+        final double heat = 0.75;
+        int toppings;
+        double size;
+        double total;
+        
+        System.out.println("Please input the desired size(inches): ");
+        size = scan.nextDouble();
+        System.out.println("Please input the amount of toppings: ");
+        toppings = scan.nextInt();
+        
+        total = size * toppings + labor + heat;
     }
 }
