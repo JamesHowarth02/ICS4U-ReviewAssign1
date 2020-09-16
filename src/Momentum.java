0cdf24e36e44c0ff984fdbddc2fbb69945ce6059
jame1850 <jame1850@ISS2TNRKH2.scdsb.pri>
2020-09-16 10:50:19 -0400

    Completed Momentum.

--- src/Momentum.java
+++ src/Momentum.java
@@ -6,14 +6,25 @@ import java.util.Scanner;
  */
 public class Momentum {
 
+    public static double persistInput(String inputText, Scanner scanobj) {
+        double numberInput = 0;
+        System.out.print(inputText);
+        try{
+            numberInput = scanobj.nextInt();
+            return numberInput;
+        }catch(Exception e) {
+            scanobj.next();
+            System.out.println("[!] Invalid input detected.");
+            return persistInput(inputText, scanobj);
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double mass, velocity, momentum;
         
-        System.out.println("Please input the mass: ");
-        mass = scan.nextDouble();
-        System.out.println("Please input the velocity: ");
-        velocity = scan.nextDouble();
+        mass = persistInput("Please input the mass: ", scan);
+        velocity = persistInput("Please input the velocity: ", scan);
         
         momentum = mass * velocity;
         System.out.println(String.format("Calculated Momentum is: %.2f", momentum));
